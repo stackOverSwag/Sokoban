1f16031325402167965a8675d27c30e10107341d
rayou-sama <118019083+rayou-sama@users.noreply.github.com>
2023-02-06 17:06:56 +0100

    Update case.java

---
+++ case.java
@@ -0,0 +1,19 @@
+public class Case extends Object{
+    private int x, y;
+    /**
+    * Methode qui affecte la valeur de son paramètre
+    * au Point this.
+    */
+    public void setX(int p) {
+    x = p;
+    }
+    public void setY(int p) {
+    y = p;
+    }
+    public int getX() {
+    return x;
+    }
+    public int getY() {
+    return y;
+    }
+};

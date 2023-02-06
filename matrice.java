c2c20ea22dd87f515ca4a2c7f4157a6f3e11f069
rayou-sama <118019083+rayou-sama@users.noreply.github.com>
2023-02-06 16:47:13 +0100

    Update matrice.java

--- matrice.java
+++ matrice.java
@@ -12,26 +12,20 @@
  */
 
 public class matrice {
-    private int n;
-    public matrice(int n){
-        int y,x;
-        for(y = 0;y <n;y++){
-            for(x=0;x<n;x++){  
-                if(x==0 || x==n-1 ||y==0 ||y==n-1){
-                    System.out.printf("#");
-                }	
-                else{
-                    System.out.printf(" ");
-                } 
-            }
-            System.out.printf("\n"); 
-        }
-    }
-    public matrice(int n, pos p, pos b, pos cp, pos cb){
-        super();
-        
-
-
-    }
+     public static void main(String args[]) {  
+        try {  
+            //constructor of file class having file as argument  
+            File file=new File("../niveaux/outlevels/1by1_clone_flower.txt");   
+            FileInputStream fis=new FileInputStream(file);     //opens a connection to an actual file  
+            System.out.println("file content: ");  
+            int r=0;  
+            while((r=fis.read())!=-1){  
+                System.out.print((char)r);      //prints the content of the file  
+            }  
+        }  
+        catch(Exception e){  
+        e.printStackTrace();  
+        }  
+    } 
 }
 

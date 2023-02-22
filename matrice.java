e3577ee2fe4044c19e538c316ad92f79755dc294
Alex Belenkiy <51411108+Russian-resident@users.noreply.github.com>
2023-02-22 14:04:09 +0100

    Update matrice.java

--- matrice.java
+++ matrice.java
@@ -12,20 +12,20 @@
  */
 
 public class matrice {
-     public static void main(String args[]) {  
-        try {  
-            //constructor of file class having file as argument  
-            File file=new File("../niveaux/outlevels/1by1_clone_flower.txt");   
-            FileInputStream fis=new FileInputStream(file);     //opens a connection to an actual file  
-            System.out.println("file content: ");  
-            int r=0;  
-            while((r=fis.read())!=-1){  
-                System.out.print((char)r);      //prints the content of the file  
-            }  
-        }  
-        catch(Exception e){  
-        e.printStackTrace();  
-        }  
-    } 
+     public static void main(String args[]) {
+        try {
+            //constructor of file class having file as argument
+            File file = new File("../niveaux/outlevels/1by1_clone_flower.txt"); 
+            FileInputStream fis = new FileInputStream(file);     //opens a connection to an actual file
+            System.out.println("file content: ");
+            int r = 0;
+            while((r = fis.read())! = -1) {
+                System.out.print((char) r);      //prints the content of the file
+            }
+        }
+        catch(Exception e) {
+          e.printStackTrace();
+        }
+    }
 }
 

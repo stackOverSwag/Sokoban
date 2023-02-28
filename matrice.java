66191081c1c9db71b604db5a3bd2ae6f6ff09ffd
Alex Belenkiy <51411108+Russian-resident@users.noreply.github.com>
2023-02-28 14:23:00 +0100

    Rayan matrice merge

--- matrice.java
+++ matrice.java
@@ -10,122 +10,42 @@
  *      'b'boite sur cible
  *      'a'personnage sur cible
  */
-/*
-public class matrice {
-     public static void main(String args[]) {
-        try {
-            //constructor of file class having file as argument
-            File file = new File("../niveaux/outlevels/1by1_clone_flower.txt"); 
-            FileInputStream fis = new FileInputStream(file);     //opens a connection to an actual file
-            System.out.println("file content: ");
-            int r = 0;
-            while((r = fis.read())! = -1) {
-                System.out.print((char) r);      //prints the content of the file
-            }
-        }
-        catch(Exception e) {
-          e.printStackTrace();
-        }
-    }
-}
-*/
 
 
-import java.util.Scanner;
-
-public class MovementExample {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int x = 0;
-        int y = 0;
-
-        System.out.println("Enter a movement direction (up, down, left, or right):");
-
-        String direction = scanner.nextLine();
-
-        switch (direction) {
-            case "up":
-                y++;
-                break;
-            case "down":
-                y--;
-                break;
-            case "left":
-                x--;
-                break;
-            case "right":
-                x++;
-                break;
-            default:
-                System.out.println("Invalid direction");
-                return;
-        }
-        
-        System.out.println("Moved " + direction + " to position (" + x + "," + y + ")");
-    }
-}
-
-In this example, the program prompts the user to enter a movement direction (up, down, left, or right), and then updates the x and y coordinates accordingly. The switch statement is used to determine the direction entered by the user, and the corresponding x and y values are updated using the ++ and -- operators. Finally, the new position is printed to the console.
-
-Note that this is just a simple example, and you can build on this to create more complex movements or games.
-Rayan Zeriri
-code in java a matrix that can read a file and repilcate his properties
-
-Sure, here's an example code in Java that reads a file and creates a matrix with the same properties:
-
-java
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class MatrixFromFile {
+
+public class matrice {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("matrix.txt");
-        Scanner scanner = new Scanner(file);
+      File file = new File("../niveaux/outlevels/1by1_clone_flower.txt");
+      Scanner scanner = new Scanner(file);
+      String monde=scanner.next();
+      // Get the number of rows and columns from the file
+      
+      int numRows = scanner.nextInt();
+      int numCols = numRows;
+      scanner.nextLine(); // Move the scanner to the next line
 
-        // Get the number of rows and columns from the file
-        int numRows = scanner.nextInt();
-        int numCols = scanner.nextInt();
-        scanner.nextLine(); // Move the scanner to the next line
+      // Create a matrix with the same properties as the file
+      char[][] matrix = new char[numRows][numCols];
 
-        // Create a matrix with the same properties as the file
-        int[][] matrix = new int[numRows][numCols];
+      // Fill the matrix with data from the file
+      for (int i = 0; i < numRows; i++) {
+          String line = scanner.nextLine();
+          for (int j = 0; j < numCols; j++) {
+              matrix[i][j] = line.charAt(j);
+          }
+      }
 
-        // Fill the matrix with data from the file
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-            scanner.nextLine(); // Move the scanner to the next line
-        }
-
-        // Print the matrix to the console
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+      // Print the matrix to the console
+      for (int i = 0; i < numRows; i++) {
+          for (int j = 0; j < numCols; j++) {
+              System.out.print(matrix[i][j] + " ");
+          }
+          System.out.println();
+      }
     }
 }
-
-/*In this example, we create a File object and a Scanner object to read the file. We get the number of rows and columns from the file, and use them to create a matrix with the same properties. We then fill the matrix with data from the file using nested for loops. Finally, we print the matrix to the console.
-
-Assuming the input file (matrix.txt) looks like this:
-
-3 4
-1 2 3 4
-5 6 7 8
-9 10 11 12
-
-the output will be:
-
-1 2 3 4 
-5 6 7 8 
-9 10 11 12 
-
-Note that you should handle exceptions appropriately, such as FileNotFoundException which may be thrown if the file does not exist or cannot be opened. Also, you can modify the code to handle different types of data in the file (e.g. strings or floats), or to write the matrix to a new file instead of printing to the console.
-*/

bb5f52f08e550c4dcd5ec08c4a245ab92b073913
rayou-sama <118019083+rayou-sama@users.noreply.github.com>
2023-03-07 19:20:57 +0100

    Add files via upload

---
+++ Graphic.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Graphic extends JFrame implements Runnable {
+
+    // Screen settings
+
+    final int caseSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread(){}
+
+    public Graphic() {
+
+        this.setTitle("Java Game");
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setDoubleBuffered(true);
+
+    }
+
+    @Override
+    public void run() { // Game Loop!
+
+        while(null != gameThread) {
+
+            // 1 Update character position.
+            update();
+
+            // 2 Draw the screen with updated information.
+            repaint();
+
+        }
+
+    }
+
+    public void update() {
+        // Leaving emppty for now!
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g); // used whenever paintComponent is used.
+
+        Graphics2D g2 = (Graphics2D) g; // A CAST?!?!? (I failed OOP)
+                                        // Graphics2D has some functions we'll need later :)
+
+        g2.setColor(Color.white); // Our object
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose(); // Memory saving technique
+    }
+
+}
---
+++ KeyHandler.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class KeyHandler implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}

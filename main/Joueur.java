7359cfdf19eadbea6ee100acad3c0fb8dc150cff
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2023-03-09 21:45:52 +0100

    RAYAN updated Joueur!

--- main/Joueur.java
+++ main/Joueur.java
@@ -1,4 +1,4 @@
-/*
+	/*
  * * Objectif mouvements:
  * modifier cordonnées joueur et boite (x,y)
  * aller sur une case designé efficacement si possible
@@ -7,31 +7,52 @@
  *      +estlibre(p,d) pour savoir si le joueur p 
  *      peut se deplacer dans la direction d sanq bouger de caisse
  *      +suivant(p,d) modifie position de p si estlibre vrai
-
  */
-
-public class joueur implements Direction{
-
+public class joueur extends Case implements Direction{
+    private int x, y;
     public joueur(int x, int y){
-
+        this.x = x;
+        this.y = y;
     }
     
-    public mouvements(Case j, Direction s) {
-        switch(s) {
+    public suivant(Case j, Direction s) {
+        if(estlibre){
+            switch(s) {
             case NORD:
+                if(matrix[getX()][getY() + 1]="#") break;
                 j.setY(getY() + 1);
                 break;
             case SUD:
+                if(matrix[getX()][getY() - 1]="#") break;
                 j.setY(getY() - 1);
                 break;
             case EST:
+                if(matrix[getX() + 1][getY()]="#") break;
                 j.setX(getX() + 1);
                 break;
             case OUEST:
+                if(matrix[getX() - 1][getY()]="#") break;
                 j.setX(getX() - 1);
                 break;
+            }
         }
+        
+    }
+    public boolean estlibre(Case j, Direction s) {
+        switch(s) {
+            case NORD:
+                if(matrix[getX()][getY() + 1]="B") return faux;
+            case SUD:
+                if(matrix[getX()][getY() - 1]="B") return faux;
+            case EST:
+                if(matrix[getX() + 1][getY()]="B") return faux;
+            case OUEST:
+                if(matrix[getX() - 1][getY()]="B") return faux;
+        }
+        return vrai;
+    }
+    public fin(Case j, Case c){
+        if(j.getX()==c.getX() && j.getY()==c.getY()) matrix[j.getX()][j.getY()]="a";
     }
 }
-
  

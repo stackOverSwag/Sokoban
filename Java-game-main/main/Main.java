8c132b377a6c6c1a819fdee3ca07429cb219d56a
rayou-sama <118019083+rayou-sama@users.noreply.github.com>
2023-04-01 02:19:32 +0200

    Add files via upload
    just add this main not even the one before and remind me to discuss about the arguments of the functions

---
+++ Java-game-main/main/Main.java
@@ -0,0 +1,140 @@
+import javax.lang.model.element.Element;
+import javax.swing.*;
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main (String[] args) {
+        
+        /*JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread(); // starts the game!
+        */
+        //en attendant que ce soit operationel
+        
+        
+        
+        
+        Matrice matrice = new Matrice("1by1_clone_flower.txt");
+        Joueur p= new Joueur(32,32,0,0);
+        Direction s=Direction.NORD;
+        System.out.println(matrice.toString());
+        while(!victory(matrice)){
+           suivant(p,s,matrice);
+        }
+        System.out.println("GG well play you are a Top G");
+
+    }
+    public static boolean suivant(Joueur p, Direction s, Matrice matrice) {
+        Joueur Joueur;
+        if(estlibre(p,s,matrice)){
+            switch(s) {
+            case NORD:
+                if(matrice.getElement(p.getX(),p.getY()+1)=='#') return false;
+
+                if(matrice.getElement(p.getX(),p.getY()+1)=='@')
+                    matrice.setElement(p.getX(),p.getY()+1,'a');
+                else
+                    matrice.setElement(p.getX(),p.getY()+1,'A');
+
+                    if(matrice.getElement(p.getX(),p.getY())=='a')
+                    matrice.setElement(p.getX(),p.getY(),'@');
+                else
+                    matrice.setElement(p.getX(),p.getY(),' ');
+                p.setY(p.getY() + 1);
+                break;
+
+            case SUD:
+                if(matrice.getElement(p.getX(),p.getY()-1)=='#') return false;
+
+                if(matrice.getElement(p.getX(),p.getY()-1)=='@')
+                    matrice.setElement(p.getX(),p.getY()-1,'a');
+                else
+                    matrice.setElement(p.getX(),p.getY()-1,'A');
+
+                    if(matrice.getElement(p.getX(),p.getY())=='a')
+                    matrice.setElement(p.getX(),p.getY(),'@');
+                else
+                    matrice.setElement(p.getX(),p.getY(),' ');
+                p.setY(p.getY() - 1);
+                break;
+
+            case EST:
+                if(matrice.getElement(p.getX()+1,p.getY())=='#') return false;
+
+                if(matrice.getElement(p.getX()+1,p.getY())=='@')
+                    matrice.setElement(p.getX()+1,p.getY(),'a');
+                else
+                    matrice.setElement(p.getX()+1,p.getY(),'A');
+
+                    if(matrice.getElement(p.getX(),p.getY())=='a')
+                    matrice.setElement(p.getX(),p.getY(),'@');
+                else
+                    matrice.setElement(p.getX(),p.getY(),' ');    
+                p.setX(p.getX() + 1);
+                break;
+
+            case OUEST:
+                if(matrice.getElement(p.getX()-1,p.getY())=='#') return false;
+
+                if(matrice.getElement(p.getX()-1,p.getY())=='@')
+                    matrice.setElement(p.getX()-1,p.getY(),'a');
+                else
+                    matrice.setElement(p.getX()-1,p.getY(),'A');
+
+                if(matrice.getElement(p.getX(),p.getY())=='a')
+                    matrice.setElement(p.getX(),p.getY(),'@');
+                else
+                    matrice.setElement(p.getX(),p.getY(),' ');
+                p.setX(p.getX() - 1);
+                break;
+            }
+            matrice.toString();
+            return true;
+        }
+        return false;
+        
+    }
+    
+    public static boolean estlibre(Joueur p, Direction s, Matrice matrice) {
+        switch(s) {
+            case NORD:
+                if(matrice.getElement(p.getX(),p.getY()+1)=='B') return false;
+                break;
+            case SUD:
+                if(matrice.getElement(p.getX(),p.getY()-1)=='B') return false;
+                break;
+            case EST:
+                if(matrice.getElement(p.getX()+1,p.getY())=='B') return false;
+                break;
+            case OUEST:
+                if(matrice.getElement(p.getX()-1,p.getY())=='B') return false;
+                break;
+        }
+        return true;
+    }
+
+    public static boolean victory(Matrice matrice){
+        for(int i=0;i<matrice.getRows();i++){
+            for(int j=0;j<matrice.getCols();j++){
+            if(matrice.getElement(i, j)=='@') return false;
+            }
+        }
+        return true;
+    }
+  
+    
+
+}
+
---
+++ Java-game-main/main/Matrice.java
@@ -0,0 +1,111 @@
+/*
+ *  * objectif matrice:
+ * nom du monde est une lettre suivi d'un chiffre taille
+ * creer un carré vide de n lignes et n colonnes avec les cases:
+ *      ' 'vide
+ *      '#'mur
+ *      'B'boite
+ *      'A'personnage
+ *      '@'cible
+ *      'b'boite sur cible
+ *      'a'personnage sur cible
+ */
+
+ import java.io.File;
+ import java.io.FileNotFoundException;
+ import java.util.Scanner;
+ 
+ 
+ public class Matrice {
+    private char[][] matrix;
+    private int rows=0;
+    private int cols=0;
+    private char monde;
+    String fileName ="1by1_clone_flower.txt";
+    // Create a matrix with the same properties as the file
+    public Matrice(int rows, int cols){
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = new char[rows][cols];
+    }
+
+    public Matrice(String file) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            monde = scanner.next().charAt(0);
+            rows = scanner.nextInt();
+            cols = rows;
+            String line = scanner.nextLine();
+            this.matrix = new char[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                line = scanner.nextLine();
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = line.charAt(j);
+                    if (matrix[i][j] == 'B')
+                        new Boite(32, 32, i, j);
+                    if (matrix[i][j] == 'A')
+                        new Joueur(32, 32, i, j);
+                    if (matrix[i][j] == '@')
+                        new Cible (32, 32, i, j);
+                    if (matrix[i][j] == '#')
+                        new Mur(32, 32, i, j);
+                    if (matrix[i][j] == '#')
+                        new Mur(32, 32, i, j);
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            System.out.println("Une erreur s'est produite lors de la lecture du fichier : " + e.getMessage());
+        }
+    }
+
+    public Matrice(Matrice other) {
+        rows = other.getRows();
+        cols = other.getCols();
+        matrix = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = other.getElement(i, j);
+            }
+        }
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getRows() {
+    return rows;
+    }
+
+    public int getCols() {
+    return cols;
+    }
+
+    public char getElement(int i, int j) {
+    return matrix[i][j];
+    }
+
+    public void setElement(int i, int j, char val) {
+    matrix[i][j] = val;
+    }
+
+     
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("nom du monde : ").append(monde).append("\ntaille du monde : ").append(rows).append("\n");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sb.append(matrix[i][j]);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+        
+    }  
+     
+ }
+ 

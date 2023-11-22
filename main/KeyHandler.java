b255c3b5f9ab9e3284f22903b9df441f74b8b606
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2023-11-22 17:32:45 +0100

    Update KeyHandler.java

--- main/KeyHandler.java
+++ main/KeyHandler.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class KeyHandler implements KeyListener {
 
-    public boolean upPressed, downPressed, leftPressed, rightPressed, escape;
+    public boolean escape;
 
     @Override
     public void keyTyped(KeyEvent e) {} // NOT USED EVER
@@ -16,45 +16,10 @@ public class KeyHandler implements KeyListener {
         
         if (code == KeyEvent.VK_ESCAPE) {
             escape = true;
-        }
-        if (code == KeyEvent.VK_W || code == KeyEvent.VK_Z) {
-            upPressed = true;
-        }
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_Q) {
-            leftPressed = true;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {} // NOT USED EVER
-
-    public Direction getDirection() {
-        if (upPressed && !downPressed && !leftPressed && !rightPressed) {
-            upPressed = false;
-            return Direction.NORD;
-        } else if (!upPressed && downPressed && !leftPressed && !rightPressed) {
-            downPressed = false;
-            return Direction.SUD;
-        } else if (!upPressed && !downPressed && leftPressed && !rightPressed) {
-            leftPressed = false;
-            return Direction.OUEST;
-        } else if (!upPressed && !downPressed && !leftPressed && rightPressed) {
-            rightPressed = false;
-            return Direction.EST;
-        }
-        upPressed = false;
-        downPressed = false;
-        leftPressed = false;
-        rightPressed = false;
-        return Direction.NONE; // Default direction when no arrow keys are pressed
-    }
-    
 
     public boolean Pause(){
         boolean pause = this.escape;
